015716abb9a622802edd57fe391cfbb0b63e0e79
OksanaBuniak <oksana.bunyak@gmail.com>
2016-05-15 18:25:30 -0700

    Implemented mini music player

--- src/MiniMusicPlayer/MiniMusicPlayer3.java
+++ src/MiniMusicPlayer/MiniMusicPlayer3.java
@@ -18,7 +18,7 @@ public class MiniMusicPlayer3 {
     public void setUpGui() {
         m1 = new MyDrawPanel();
         f.setContentPane(m1);
-        f.setBounds(30,30,300,300);
+        f.setBounds(30, 30, 300, 300);
         f.setVisible(true);
     }
 
@@ -27,22 +27,24 @@ public class MiniMusicPlayer3 {
         try {
             Sequencer sequencer = MidiSystem.getSequencer();
             sequencer.open();
-            sequencer.addControllerEventListener(m1, new int[] {127});
+            sequencer.addControllerEventListener(m1, new int[]{127});
             Sequence seq = new Sequence(Sequence.PPQ, 4);
             Track track = seq.createTrack();
 
             int r = 0;
-            for (int i = 0; i < 60; i+= 4) {
+            for (int i = 0; i < 60; i += 4) {
                 r = (int) ((Math.random() * 50) + 1);
-                track.add(makeEvent(144,1,r,100,i));
-                track.add(makeEvent(176,1,127,0,i));
-                track.add(makeEvent(128,1,r,100,i + 2));
+                track.add(makeEvent(144, 1, r, 100, i));
+                track.add(makeEvent(176, 1, 127, 0, i));
+                track.add(makeEvent(128, 1, r, 100, i + 2));
             }
 
             sequencer.setSequence(seq);
             sequencer.start();
             sequencer.setTempoInBPM(120);
-        } catch (Exception ex) {ex.printStackTrace();}
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
@@ -52,11 +54,12 @@ public class MiniMusicPlayer3 {
             ShortMessage a = new ShortMessage();
             a.setMessage(comd, chan, one, two);
             event = new MidiEvent(a, tick);
-        } catch (Exception e) {}
+        } catch (Exception e) {
+        }
         return event;
     }
 
-    class MyDrawPanel extends JPanel implements ControllerEventListener{
+    class MyDrawPanel extends JPanel implements ControllerEventListener {
         boolean msg = false;
 
         public void controlChange(ShortMessage event) {
@@ -66,19 +69,19 @@ public class MiniMusicPlayer3 {
 
         public void paintComponent(Graphics g) {
             if (msg) {
-                int r = (int)(Math.random() * 250);
-                int gr = (int)(Math.random() * 250);
+                int r = (int) (Math.random() * 250);
+                int gr = (int) (Math.random() * 250);
                 int b = (int) (Math.random() * 250);
 
-                g.setColor(new Color(r,gr,b));
+                g.setColor(new Color(r, gr, b));
 
-                int ht = (int)((Math.random() * 120) + 10);
-                int width = (int)((Math.random() * 120) + 10);
+                int ht = (int) ((Math.random() * 120) + 10);
+                int width = (int) ((Math.random() * 120) + 10);
 
-                int x = (int)((Math.random() * 40) + 10);
-                int y = (int)((Math.random() * 40) + 10);
+                int x = (int) ((Math.random() * 40) + 10);
+                int y = (int) ((Math.random() * 40) + 10);
 
-                g.fillRect(x,y,ht,width);
+                g.fillRect(x, y, ht, width);
                 msg = false;
             }
         }
